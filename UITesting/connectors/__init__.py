a84d177fb3b560e8e2153f4fd4c8c92960b2c208
Denys Shutka <nden2105@gmail.com>
2021-11-27 20:47:40 +0200

    feat: expand connector logic

--- UITesting/connectors/__init__.py
+++ UITesting/connectors/__init__.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import Tuple
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -11,16 +12,16 @@ from drivers import Drivers
 LocatorType = Tuple[By, str]
 
 
-class Singleton(type):
+class SingletonMeta(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
-class SeleniumConnector(metaclass=Singleton):
+class SeleniumConnector(metaclass=SingletonMeta):
     def __init__(self, *args, **kwargs):
         self.driver: WebDriver = None
 
@@ -28,15 +29,24 @@ class SeleniumConnector(metaclass=Singleton):
     def url(self) -> str:
         return self.driver.current_url
 
+    def configure(self, waiting_time: int = 1, height: int = 1024, width: int = 768):
+        self.driver.set_window_size(height=height, width=width)
+        self.driver.implicitly_wait(waiting_time)
+        self.driver.switch_to.default_content()
+
     def navigate_url(self, site_url: str):
         self.driver.get(site_url)
+
+    def check_if_element_exists(self, locator: LocatorType) -> bool:
+        try:
+            element = self.find_element_by_locator(locator)
+            return element is not None
+        except NoSuchElementException:
+            return False
 
     def find_element_by_locator(self, locator: LocatorType) -> WebElement:
         locator_strategy, locator_name = locator
         return self.driver.find_element(locator_strategy, locator_name)
-
-    def find_element_by_css_id(self, element_id: str) -> WebElement:
-        return self.driver.find_element(By.ID, element_id)
 
     def close(self):
         self.driver.close()
